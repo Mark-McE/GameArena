46e2182007e54af1f80432017067a5c640f9ba97
Mark McElroy <m.mcelroy@lancaster.ac.uk>
2017-02-15 23:51:58 +0000

    added ball-paddle collisions, resolves y component only

--- Ball.java
+++ Ball.java
@@ -15,12 +15,16 @@ public class Ball{
   * Basic Constructor
   * @param x The starting X position of the ball
   * @param y The starting Y position of the ball
+  * @param vx The starting X velocity of the ball
+  * @param vy The starting Y velocity of the ball
   * @param diameter The diameter of the call, in pixels
   * @param col The colour of the ball, can take RGB values or common colours as strings
   */
-	public Ball(double x, double y, double diameter, String col){
+	public Ball(double x, double y, double vx, double vy, double diameter, String col){
 		position[0] = x;
 		position[1] = y;
+    velocity[0] = vx;
+    velocity[1] = vy;
 		size = diameter;
 		colour = col;
 	}	
@@ -29,24 +33,96 @@ public class Ball{
   * Checks if a ball is colliding with a paddle
   * @return true if ball is colliding with paddle, false if not
   */
-  public boolean colliding(Paddle paddle){
+  public boolean colliding(Rectangle rec){
     
-    double dx = Math.abs( this.position[0] - (double)paddle.getRectangle().getXPosition() );
-    double dy = Math.abs( this.position[1] - (double)paddle.getRectangle().getYPosition() );
+    double dx = Math.abs( this.position[0] - rec.getXPosition() );
+    double dy = Math.abs( this.position[1] - rec.getYPosition() );
     
-    if(dx > paddle.getRectangle().getWidth()/2 +size/2 ){
+    // obvious too far away to collides dealt with first
+    if(dx > rec.getWidth()/2 + this.size/2 ){
       return false;
     }
-    if(dy > paddle.getRectangle().getHeight()/2+size/2){
+    if(dy > rec.getHeight()/2+size/2){
       return false;
     }
-    // TODO
-    // ** NOT FINISHED ** 
-    // TODO
-    // ** re-model this is check if colliding with a rectangle, 
-    // and use paddle.getRectangle() when calling this method **
-    return true;
+    
+    // all checks from here will now not satisfy the above if statements
+    // this means if the ball was a rectangle, it would collide at this point
+    
+    // obvious cases of touching
+    if(dx <= rec.getWidth()/2){
+      return true;
+    }
+    if(dy <= rec.getHeight()/2){
+      return true;
+    }
+    
+    // checks for collisions with the corner of the rectangle and the ball
+    double cornerToCircleCenterDistSqr = Math.pow(dx-rec.getWidth()/2,2) +Math.pow(dy-rec.getHeight()/2,2);
+    if(cornerToCircleCenterDistSqr <= Math.pow(this.size/2,2)){
+      return true;
+    }
+    return false;
   }
+  
+  /**
+  * Resolves collisions with this ball and a passed rectangle
+  * by updating this ball's velocity vector
+  * resolved x velocity is set to a function of how far from the center
+  * of the paddle we collide
+  * resolved y velocity is that of an elastic collision
+  * @param paddle The paddle this ball is colliding with
+  */
+  public void resolvePaddleCollision(Paddle paddle){
+    Rectangle rec = paddle.getRectangle();
+    
+    double dxSigned = rec.getXPosition() - this.position[0];
+    double dySigned = rec.getYPosition() - this.position[1];
+    
+    if( Math.signum(velocity[1]) != Math.signum( dxSigned ) ){
+      this.velocity[1] *= -1;
+    }
+    
+    // TODO x velocity resolvolution
+    
+  }
+  
+  /**
+  * Checks for collisions with game arena walla and if true, resolves them
+  * @param windowWidth The width of the game arena
+  * @param windowHeight The height of the game arena
+  */
+  public void resolveWallCollisions(int windowWidth, int windowHeight){
+    // collisions with right and left wall
+    if(
+    (  this.position[0] + this.size/2 >= windowWidth
+    && this.velocity[0] > 0 )
+    ||
+    (  this.position[0] - this.size/2 <= 0
+    && this.velocity[0] < 0 ))
+    {
+      this.velocity[0] *= -1;
+    }
+    // collisions with bottom and top wall
+    if(
+    (  this.position[1] + this.size/2 >= windowHeight
+    && this.velocity[1] > 0 )
+    ||
+    (  this.position[1] - this.size/2 <= 0
+    && this.velocity[1] < 0 ))
+    {
+      this.velocity[1] *= -1;
+    }
+  }
+  
+  /**
+  * Adds the current velocity to the current position
+  */
+  public void updatePos(){
+    this.position[0] += this.velocity[0];
+    this.position[1] += this.velocity[1];
+  }
+  
   
 	/**
 	 * Obtains the current position of this Ball.
--- BrickBreakerWeek15.java
+++ BrickBreakerWeek15.java
@@ -14,6 +14,9 @@ public class BrickBreakerWeek15{
     Paddle playerPaddle = new Paddle(windowLength/2, windowHeight-30, "cyan", 50);
     playerPaddle.addPaddle(gameWindow);
     
+    Ball ball = new Ball(windowLength/2,windowHeight/2,0,5,10,"yellow");
+    gameWindow.addBall(ball);
+    
     while(true){
       if(gameWindow.rightPressed()){
         playerPaddle.move(5);
@@ -21,6 +24,14 @@ public class BrickBreakerWeek15{
       if(gameWindow.leftPressed()){
         playerPaddle.move(-5);
       }
+      
+      if( ball.colliding(playerPaddle.getRectangle() )){
+        ball.resolvePaddleCollision(playerPaddle);
+      }
+      
+      ball.resolveWallCollisions(windowLength,windowHeight);
+      
+      ball.updatePos();
       gameWindow.pause();
     }
   }
